9dcc02e23c50e650e1080560386f434050a393df
Nischit H <nischitv29@gmail.com>
2021-10-09 23:18:06 +0530

    Added modified code

--- webd/routes.py
+++ webd/routes.py
@@ -1,5 +1,12 @@
+from sqlalchemy import create_engine
+
+from sqlalchemy.testing import db
+from sqlalchemy.orm import scoped_session, sessionmaker
+from passlib.hash import sha256_crypt
+import sys
+
 from webd import app
-from flask import redirect, url_for, session, render_template
+from flask import redirect, url_for, session, render_template, request, flash
 from webd import oauth
 
 @app.route("/")
@@ -27,7 +34,7 @@ def authorize():
 def form():
     user=dict(session).get('profile',None);
     if user:
-        return render_template('form.html')
+        return render_template('relocate.html')
     else:
         return "Please Login as admin"
 
@@ -36,3 +43,78 @@ def logout():
     for key in list(session.keys()):
         session.pop(key)
     return redirect('/')
+
+
+
+engine = create_engine("mysql://root:Hosamani29m$@localhost/lab5", echo=True)
+
+#Check connection
+try:
+    conn = engine.connect()
+except Exception as e:
+    print('Connection Failed\nError Details:', e)
+    sys.exit(1)
+conn.close()
+
+db = scoped_session(sessionmaker(bind=engine))
+log = False
+
+@app.route("/admin", methods=["GET", "POST"])
+def admin():
+    print(log)
+    if request.method == "POST":
+        email = request.form.get("email")
+        name = request.form.get("name")
+        dept = request.form.get("dept")
+        pwd = request.form.get("pwd")
+        secure_password = sha256_crypt.encrypt(str(pwd))
+        print(name, email, dept, pwd)
+
+        table_data = db.execute("SELECT email FROM faculty WHERE email =:email",
+        {"email":email}).fetchone()
+
+        if table_data == None:
+            db.execute("INSERT INTO faculty(email, facultyname, dept, password) VALUES(:email, :facultyname, :dept, :password)",
+            {"email":email,"facultyname":name, "dept": dept, "password":secure_password})
+            db.commit()
+            flash("Faculty Registered Successfully", "success")
+            return redirect(url_for("admin"))
+
+        else:
+            flash("Faculty already Registered", "danger")
+            return redirect(url_for("admin"))
+
+    return render_template("admin_form.html")
+
+@app.route("/faculty", methods=["GET", "POST"])
+def faculty():
+    if request.method == "POST":
+        email = request.form.get("email")
+        pwd = request.form.get("pwd")
+
+        table_data = db.execute("SELECT email FROM faculty WHERE email =:email",
+                                {"email": email}).fetchone()
+        password_data = db.execute("SELECT password FROM faculty WHERE email =:email",
+                                   {"email": email}).fetchone()
+
+        if table_data is None:
+            flash("Faculty does not exist!", "error")
+            return render_template("faculty_form.html")
+        else:
+            for pass_data in password_data:
+                if sha256_crypt.verify(pwd, pass_data):
+                    session = True
+                    info = db.execute("SELECT * FROM faculty WHERE email =:email",
+                                            {"email": email}).fetchone()
+                    print(info[1], type(info))
+                    return render_template("faculty_form.html", email=info[0], name=info[1], dept=info[2])
+                else:
+                    flash("Incorrect Password", "error")
+                    return render_template("faculty_form.html")
+
+    return render_template("faculty_form.html")
+
+
+
+
+
